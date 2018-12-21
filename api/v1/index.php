885fcef8b5062bc238002fee1a51a80b22ab838c
Haqz <maszynista91@gmail.com>
2018-12-21 21:29:39 +0100

    0.3.1b bug fix

--- api/v1/index.php
+++ api/v1/index.php
@@ -1,31 +1,30 @@
 <?php
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
-
+header('Content-Type:application/json');
 require '../../vendor/autoload.php';
-
 $db = new SPC\Database('../../messaging.sqlite3');
 $app = new \Slim\App;
 $app->get('/getPost/{id}', function (Request $request, Response $response, array $args) {
+    
+    
     global $db;
     $id = $args['id'];
     try{
         $post = $db->getOne($id, 'messages');
-        $post = json_encode($post);
     } catch (Exception $ex){
         echo $ex;
     }
-    return htmlspecialchars($post);
+    return $response->withJson($post, 201);
 });
 $app->get('/getAllPosts', function (Request $request, Response $response, array $args) {
+    
     global $db;
     try{
         $post = $db->getAll('messages');
-        $post = json_encode($post);
     } catch (Exception $ex){
         echo $ex;
     }
-
-    return htmlspecialchars($post);
+    return $response->withJson($post, 201);
 });
 $app->run();
