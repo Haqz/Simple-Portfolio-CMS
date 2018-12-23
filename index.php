53e53dcec756ca1ac32fd3c24a03f76479f716b8
Raloseq <45914275+Raloseq@users.noreply.github.com>
2018-12-23 17:23:33 +0100

    message

--- index.php
+++ index.php
@@ -38,12 +38,12 @@ try{
   ?>
 </head>
 <body>
-<nav class="navigation navbar navbar-expand-lg navbar-light bg-light sticky-top navigation">
+<nav class="navigation navbar navbar-expand-lg navbar-dark bg-dark sticky-top navigation">
   <a class="navbar-brand" href="#">
     <img src="prof.png" alt="Logo haqz"><span id="nav-span">Haqz</span>
   </a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
+    <span class="menu-btn navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse nav-list-left text-left" id="navbarNav">
     <ul class="navbar-nav ml-auto">
--- menuscroll.js
+++ menuscroll.js
@@ -1,19 +1,26 @@
 const navbar = document.querySelector('.navigation');
-const listItem = document.querySelectorAll('.nav-link');
+const div = document.querySelector('#navbarNav');
+const listItem = div.querySelectorAll('.nav-link');
 const navSpan = document.querySelector('#nav-span');
 window.addEventListener('scroll', function (e) {
     const lastPosition = window.scrollY;
     if (lastPosition > 50) {
         navbar.classList.add('active');
-        listItem.classList.add('active-a');
+        listItem.forEach(function (e) {
+            e.classList.add('active-a');
+        });
         navSpan.classList.add('span-active');
     } else if (navbar.classList.contains('active')) {
         navbar.classList.remove('active');
-        listItem.classList.remove('active-a');
+        listItem.forEach(function (e) {
+            e.classList.remove('active-a');
+        });
         navSpan.classList.remove('span-active');
     } else {
         navbar.classList.remove('active');
-        listItem.classList.remove('active-a');
+        listItem.forEach(function (e) {
+            e.classList.rempve('active-a');
+        });
         navSpan.classList.remove('span-active');
     }
 });
