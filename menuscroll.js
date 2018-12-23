30482922181637df329e4e0790ef88c59e8453c5
Raloseq <45914275+Raloseq@users.noreply.github.com>
2018-12-23 16:30:30 +0100

    0.4.3 - style update

--- menuscroll.js
+++ menuscroll.js
@@ -2,7 +2,7 @@ const navbar = document.querySelector('.navigation');
 const listItem = document.querySelectorAll('.nav-link');
 const navSpan = document.querySelector('#nav-span');
 window.addEventListener('scroll', function (e) {
-    const lastPosition = window.scrollY
+    const lastPosition = window.scrollY;
     if (lastPosition > 50) {
         navbar.classList.add('active');
         listItem.classList.add('active-a');
@@ -16,4 +16,4 @@ window.addEventListener('scroll', function (e) {
         listItem.classList.remove('active-a');
         navSpan.classList.remove('span-active');
     }
-})
+});
