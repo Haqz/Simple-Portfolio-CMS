5c312d63b4b04eda37f1253a0c333de9faf937d2
Raloseq <45914275+Raloseq@users.noreply.github.com>
2019-01-09 20:58:34 +0100

    0.4.2b - style update

--- slidein.js
+++ slidein.js
@@ -2,7 +2,6 @@ const slideInProjects = document.querySelectorAll('.slide-in');
 
 function slideProject(e) {
     const windowPosition = window.scrollY;
-    console.log(windowPosition);
     slideInProjects.forEach(slide => {
         if (windowPosition > 2200) {
             slide.classList.add('slide-in-project');
