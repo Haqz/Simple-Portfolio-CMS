8ab7fd43b4742ee4302bc0ffccf66a26d056e580
Haqz <maszynista91@gmail.com>
2019-04-23 11:13:30 +0200

    0.6-pre article.php updated, works

--- article.php
+++ article.php
@@ -2,20 +2,26 @@
 
 require 'vendor/autoload.php';
 require 'src/Autoloader.php';
-try{
-      
-      $db = new SPC\Database();
-      $settings = new SPC\Settings($db);
-      $post = new SPC\Post($db);
-      $user = new SPC\User($db);
-      $scss = new scssc;
-}catch(Exception $e){
-      echo $e->getMessage();
+try {
+    $db = new SPC\Database();
+    $settings = new SPC\Settings($db);
+    $post = new SPC\Post($db);
+    $user = new SPC\User($db);
+    $scss = new scssc;
+} catch (Exception $e) {
+    echo $e->getMessage();
 }
 $scss->setImportPaths("Style/scss/");
 echo "<style>".$scss->compile('@import "main.scss"')."</style>";
 
+
+
 $file = basename(__FILE__, '.php');
 require __DIR__ . '/TwigBootstrap.php';
-echo $twig->render('_'.$file.'.html');
-
+if (isset($_GET['id']) && is_numeric($_GET['id'])) {
+    $postsData = $post->getLatestPostsData();
+    $actualPost = $post->findPostData($_GET['id']);
+    echo $twig->render('_'.$file.'.html', ['posts'=>$postsData, 'post'=>$actualPost]);
+} else {
+    echo $twig->render('_404.html');
+}
