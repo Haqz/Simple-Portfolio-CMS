91bad9f6d6cd2948fa70f6525e7812da642a24da
Haqz <maszynista91@gmail.com>
2019-10-27 23:06:10 +0100

    index update

--- menuscroll.js
+++ menuscroll.js
@@ -1,5 +1,5 @@
 const navbar = document.querySelector('.navigation');
-window.addEventListener('scroll', function (e) {
+window.addEventListener('scroll', function(e) {
     const lastPosition = window.scrollY;
     if (lastPosition > 50) {
         navbar.classList.add('active');
